464ada1d661322082055a3d8045c58a1554696b2
matvey-27 <neutral_notes@tutamail.com>
2025-04-07 21:08:08 +0300

    temp save

--- init.lua
+++ init.lua
@@ -1,3 +1,53 @@
 -- Установка VimPlug
 -- sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 --     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+home=os.getenv("HOME")
+package.path = home .. "/.config/nvim/?.lua;" .. package.path
+
+-- темы
+Plug('rebelot/kanagawa.nvim')
+Plug('morhetz/gruvbox')
+
+-- плагин nvim-tree и плагин для поддержки иконок файлов
+Plug('kyazdani42/nvim-tree.lua')
+Plug('kyazdani42/nvim-web-devicons')
+
+-- BarBar - вкладки файлов
+Plug('romgrk/barbar.nvim')
+
+-- Lualine - строка статуса 
+Plug('nvim-lualine/lualine.nvim') 
+
+-- TreeSitter - дерево кода
+Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
+
+-- LSP-сервер и mason
+Plug('neovim/nvim-lspconfig')
+Plug('williamboman/mason.nvim')
+Plug('williamboman/mason-lspconfig.nvim')
+Plug('hrsh7th/nvim-cmp')
+Plug('hrsh7th/cmp-nvim-lsp')
+-- Plug("'VonHeikemen/lsp-zero.nvim'")
+
+vim.call('plug#end')
+
+
+dofile"theme.lua"
+
+require"vimtree"
+
+-- require"barbar"
+
+require"lua_line"
+
+require"treesitter"
+
+require"lsp"
+
+require"common"
---
+++ lua/lsp-config.lua
@@ -0,0 +1,80 @@
+local lsp_zero = require('lsp-zero').preset({
+    name = 'recommended',
+    set_lsp_keymaps = true,
+    manage_nvim_cmp = true,
+  })
+  
+  -- Настройка keymaps
+  lsp_zero.on_attach(function(client, bufnr)
+    local opts = {buffer = bufnr, remap = false}
+    
+    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+  end)
+  
+  -- Настройка Mason
+  require('mason').setup({
+    ui = {
+      icons = {
+        package_installed = "✓",
+        package_pending = "➜",
+        package_uninstalled = "✗"
+      }
+    }
+  })
+  
+  -- Настройка LSP серверов
+  require('mason-lspconfig').setup({
+    ensure_installed = {'lua_ls'},  -- Автоустановка lua_ls
+    handlers = {
+      lsp_zero.default_setup,
+      lua_ls = function()
+        require('lspconfig').lua_ls.setup({
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = {'vim'}
+              },
+              workspace = {
+                checkThirdParty = false
+              }
+            }
+          }
+        })
+      end,
+    }
+  })
+  
+  -- Настройка автодополнения
+  local cmp = require('cmp')
+  local cmp_select = {behavior = cmp.SelectBehavior.Select}
+  local cmp_mappings = lsp_zero.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({select = true}),
+    ['<C-Space>'] = cmp.mapping.complete(),
+  })
+  
+  cmp.setup({
+    mapping = cmp_mappings,
+    sources = {
+      {name = 'nvim_lsp'},
+      {name = 'luasnip'},
+    },
+    snippet = {
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body)
+      end,
+    },
+  })
+  
+  -- Инициализация LSP
+  lsp_zero.setup()
