1b9c2464384ffd87a68e75dd79b8fcc1cea5cf2e
matvey-27 <neutral_notes@tutamail.com>
2025-06-27 11:50:54 +0300

    update settings

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -1,125 +1,95 @@
--- lsp-config.lua
 local lsp_zero = require('lsp-zero').preset({
   name = 'recommended',
-  set_lsp_keymaps = true,
-  manage_nvim_cmp = true,
+  set_lsp_keymaps = false,
+  manage_nvim_cmp = false,
 })
 
--- Что такое loader?
--- В Neovim "loader" (загрузчик) - это механизм, который управляет:
--- 1. Загрузкой модулей Lua (через require)
--- 2. Инициализацией плагинов
--- Например, когда вы пишете require('lsp-zero'), Neovim ищет модуль в:
--- - runtimepath (папки lua/)
--- - установленных плагинах
-
--- Настройка keymaps
-lsp_zero.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false, desc = 'LSP: '}
-  
-  -- Просмотр документации
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-  vim.keymap.set('n', '<leader>dk', function()
-    local float = vim.lsp.util.open_floating_preview(
-      vim.lsp.util.convert_input_to_markdown_lines(
-        vim.lsp.buf_get_clients()[1].request('textDocument/hover', {
-          textDocument = vim.lsp.util.make_text_document_params(),
-          position = vim.lsp.util.make_position_params()
-        })
-      ), 'markdown')
-    vim.api.nvim_buf_set_option(float.bufnr, 'filetype', 'markdown')
-  end, opts)
-
-  -- Навигация
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-  vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
-
-  -- Работа с ошибками
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-  vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, opts)
-  vim.keymap.set('n', '<leader>dl', function()
-    vim.diagnostic.setloclist({open = true})
-  end, opts)
-
-  -- Другие действия
-  vim.keymap.set('n', '<leader>vrn', vim.lsp.buf.rename, opts)
-  vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, opts)
-  vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
-end)
-
--- Настройка Mason (менеджер LSP серверов)
-require('mason').setup({
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗"
-    }
-  }
-})
-
--- Настройка LSP серверов
+-- Настройка Mason без автоматической установки
+require('mason').setup()
 require('mason-lspconfig').setup({
-  ensure_installed = {'lua_ls'},
-  handlers = {
-    lsp_zero.default_setup,
-    lua_ls = function()
-      require('lspconfig').lua_ls.setup({
-        settings = {
-          Lua = {
-            diagnostics = {globals = {'vim'}},
-            workspace = {checkThirdParty = false},
-            telemetry = {enable = false}
-          }
-        }
-      })
-    end,
-  }
+  -- Не указываем automatic_installation
+  ensure_installed = { 'tsserver' } -- список только нужных серверов
 })
 
--- Настройка автодополнения
+local lspconfig = require('lspconfig')
+
+-- Получаем список установленных серверов Mason
+local servers = require('mason-lspconfig').get_installed_servers()
+
+for _, server in ipairs(servers) do
+  lspconfig[server].setup({
+    on_attach = function(client, bufnr)
+      local opts = {buffer = bufnr}
+
+      -- Настройка ключевых комбинаций
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+      vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
+
+      -- Навигация по ошибкам
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+      vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, opts)
+      vim.keymap.set('n', '<leader>dl', function()
+        vim.diagnostic.setloclist({open=true})
+      end, opts)
+
+      -- Работа с rename и code actions
+      vim.keymap.set('n', '<leader>vrn', vim.lsp.buf.rename, opts)
+      vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, opts)
+
+      -- Подсказки при вводе (в insert режиме)
+      vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    end,
+    flags = {
+      debounce_text_changes = 150,
+    },
+    -- Можно добавить настройки для конкретных серверов при необходимости
+  })
+end
+
+-- Настройка автодополнения с nvim-cmp
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
   sources = {
-    {name = 'nvim_lsp'},
-    {name = 'luasnip'},
+    {name='nvim_lsp'},
+    {name='luasnip'},
   },
   mapping = {
-    ['<CR>'] = cmp.mapping.confirm({select = true}),
+    ['<CR>'] = cmp.mapping.confirm({ select=true }),
     ['<Tab>'] = cmp_action.luasnip_supertab(),
     ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
     ['<C-f>'] = cmp_action.luasnip_jump_forward(),
     ['<C-b>'] = cmp_action.luasnip_jump_backward(),
     ['<C-Space>'] = cmp.mapping.complete(),
   },
-  snippet = {
-    expand = function(args)
+  snippet={
+    expand=function(args)
       require('luasnip').lsp_expand(args.body)
     end,
   },
 })
 
--- Инициализация LSP
+-- Включение настроек lsp-zero
 lsp_zero.setup()
 
--- Дополнительные настройки диагностики
+-- Настройки диагностики
 vim.diagnostic.config({
-  virtual_text = true,
-  signs = true,
-  update_in_insert = false,
-  underline = true,
-  severity_sort = true,
-  float = {
-    focusable = false,
-    style = 'minimal',
-    border = 'rounded',
-    source = 'always',
-    header = '',
-    prefix = '',
-  },
+  virtual_text=true,
+  signs=true,
+  update_in_insert=false,
+  underline=true,
+  severity_sort=true,
+  float={
+    focusable=false,
+    style='minimal',
+    border='rounded',
+    source='always',
+    header='',
+    prefix='',
+  }
 })
--- lua/theme.lua
+++ lua/theme.lua
@@ -1,2 +1,2 @@
-vim.cmd.colorscheme("everforest")
--- vim.cmd.colorscheme("kanagawa")
+-- vim.cmd.colorscheme("everforest")
+vim.cmd.colorscheme("kanagawa")
